20a11dba66d98ae27948456394828c0535514028
pinggooo <69897315+pinggooo@users.noreply.github.com>
2021-01-20 01:16:40 +0900

    Add files via upload

---
+++ Baekjoon_2869.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Baekjoon_2869 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        br.close();
+
+        int day = (V - B) / (A - B);
+        if ((V - B) % (A - B) != 0) {
+            day++;
+        }
+        System.out.println(day);
+    }
+}
